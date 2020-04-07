fba4e3c7214d22e9fb03f223eb662fad0e818041
Manuel Miranda <3057476220301@ingenieria.usac.edu.gt>
2020-04-06 22:52:21 -0600

    Token y Lexico
    Agregada clase Token e inicio del analisis lexico

--- Practica2/src/app/Classes/Token.ts
+++ Practica2/src/app/Classes/Token.ts
@@ -1,6 +1,157 @@
-export class Token{
 
-    constructor(){
-        
+export class Token{
+    
+    /*TokenType = {
+        Reserved Words
+            WR_CLASS: 'class',
+            WR_VOID: 'void',
+            WR_MAIN: 'main',
+            WR_CONSOLE: 'Console',
+            WR_WRITE: 'Write',
+            WR_IF: 'if',
+            WR_ELSE: 'else',
+            WR_SWITCH: 'switch',
+            WR_CASE: 'case',
+            WR_BREAK: 'break',
+            WR_DEFAULT: 'default',
+            WR_DO: 'do',
+            WR_WHILE: 'while',
+            WR_FOR: 'for',
+            WR_RETURN: 'return',
+            WR_CONTINUE: 'continue',
+            WR_TRUE: 'true',
+            WR_FALSE: 'false',        
+                Types 
+            WR_INT: 'int',
+            WR_DOUBLE: 'double',
+            WR_CHAR: 'char',
+            WR_BOOL: 'bool',
+            WR_STRING: 'string',
+                End of Types 
+        End of Reserved Words
+        Symbols
+            S_OPEN_KEY: '{',
+            S_CLOSE_KEY: '}',
+            S_OPEN_PARENTHESIS: '(',
+            S_CLOSE_PARENTHESIS: ')',
+            S_COMMA: ',',
+            S_SEMICOLON: ';',
+            S_POINT: '.',
+            S_TWO_POINTS: ':',        
+            S_EQUALS: '=',
+            S_PLUS: '+',
+            S_MINUS: '-',
+            S_PRODUCT: '*',
+            S_DIVISION: '/',
+            S_NOT: '!',
+            INCREMENT_OR_DECREMENT: 'Incremento o Decremento',
+            S_INCREMENT: '++',
+            S_DECREMENT: '--',
+                Condition Symbols
+            CONDITION: 'Condicion',
+            S_EQUALS_EQUALS: '==',
+            S_MAJOR: '>',
+            S_LESS: '<',
+            S_MAJOR_EQUALS: '>=',
+            S_LESS_EQUALS: '<=',
+            S_DIFFERENT: '!=',        
+        End of Symbols
+        INTEGER: 'Entero',
+        DECIMAL: 'Decimal',
+        STRING: 'String',
+        ID: 'ID',
+        COMMENT: 'Comment',
+        UNKNOWN: 'Desconocido',
+        LAST: 'Ultimo'
+    };*/
+
+    type: string;
+    lexeme: string;
+    row: string;
+    column: string;
+
+    constructor(_type: string, _lexeme: string, _row: string, _column: string){
+        if(_type == "ID"){
+            switch(_lexeme){
+                case "class":
+                    this.type = "WR_CLASS";
+                    break;
+                case "void":
+                    this.type = "WR_VOID"
+                    break;
+                case "main":
+                    this.type = "WR_MAIN";
+                    break;
+                case "Console":
+                    this.type = "WR_CONSOLE";
+                    break;
+                case "Write":
+                    this.type = "WR_WRITE"
+                    break;
+                case "if":
+                    this.type = "WR_IF";
+                    break;
+                case "else":
+                    this.type = "WR_ELSE";
+                    break;
+                case "switch":
+                    this.type = "WR_SWITCH";
+                    break;
+                case "case":
+                    this.type = "WR_CASE";
+                    break;
+                case "break":
+                    this.type = "WR_BREAK";
+                    break;
+                case "default":
+                    this.type = "WR_DEFAULT";
+                    break;
+                case "do":
+                    this.type = "WR_DO";
+                    break;
+                case "while":
+                    this.type = "WR_WHILE";
+                    break;
+                case "for":
+                    this.type = "WR_FOR";
+                    break;
+                case "return":
+                    this.type = "WR_RETURN";
+                    break;
+                case "continue":
+                    this.type = "WR_CONTINUE";
+                    break;
+                case "true":
+                    this.type = "WR_TRUE";
+                    break;
+                case "false":
+                    this.type = "WR_FALSE";
+                    break;
+                /* Types */
+                case "int":
+                    this.type = "WR_INT";
+                    break;
+                case "double":
+                    this.type = "WR_DOUBLE";
+                    break;
+                case "char":
+                    this.type = "WR_CHAR";
+                    break;
+                case "bool":
+                    this.type = "WR_BOOL";
+                    break;
+                case "string":
+                    this.type = "WR_STRING";
+                    break;
+                default:
+                    this.type = "ID";
+                    break;
+            }
+        }else{
+            this.type = _type;
+        }        
+        this.lexeme = _lexeme;
+        this.row = _row;
+        this.column = _column;
     }
 }
--- Practica2/src/app/Classes/lexicalAnalyzer.ts
+++ Practica2/src/app/Classes/lexicalAnalyzer.ts
@@ -1,7 +1,42 @@
+import { Token } from './Token'
+import { stringify } from 'querystring';
+
 export class LexicalAnalyzer{
 
+    tokenList: Array<Token>;
+    row: number;
+    col: number;
+    colF: number;
+    state: number;
+    txtAux: string;
 
     constructor(){
-        
+        this.row = this.col = this.colF = 1;
+    }
+
+    scanText(_text:string): Array<Token> {
+                
+        this.state = 0;
+
+        for(let char of _text){
+            switch(this.state){
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+
+        return this.tokenList;
+    }
+
+    addToken(_tokenType: string){
+
     }
 }
