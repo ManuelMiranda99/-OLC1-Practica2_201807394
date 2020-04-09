036978cf4d9bdaf6a87b7d5c6196bb2d21011400
Manuel Miranda <3057476220301@ingenieria.usac.edu.gt>
2020-04-08 18:44:16 -0600

    Cambios en gramatica
    Cambios en gramatica y agregacion de la clase traductora

---
+++ Practica2/src/app/Classes/ToPython.ts
@@ -0,0 +1,7 @@
+export class ToPython{
+
+    constructor(){
+        
+    }
+
+}
--- Practica2/src/app/Classes/syntacticAnalizer.ts
+++ Practica2/src/app/Classes/syntacticAnalizer.ts
@@ -1,5 +1,13 @@
+import { Token } from './Token';
+
 export class SyntacticAnalyzer{
 
+    tokenList: Array<Token> = new Array<Token>();
+    errorsList: Array<string> = new Array<string>();
+    index: number = 0;
+    tabs: number = 0;
+    sintacticError: boolean = false;
+    errorF: boolean = false;
 
     constructor(){
         
